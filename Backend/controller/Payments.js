1b06be58ea5c64d5c65e29ee07a33c6793d78c49
Mayank <mynk@mynk>
2025-01-18 14:34:18 +0530

    added controller for signature verification of razorpay payment

--- Backend/controller/Payments.js
+++ Backend/controller/Payments.js
@@ -31,24 +31,109 @@ exports.capturePayments=async(req,res)=>{
         }
         //user already paid for the course
         const uid=mongoose.Types.ObjectId(userid);
-        if(Course.StudentsEnrolled.includes(uid)){
+        if(Course.StudentsEnrolled.some(studentId => studentId.equals(uid))){
             return res.status(400).json({
                 success:false,
                 message:"ALready enrolled"
             })
         }
 
-    }catch(err){
 
+
+    }catch(err){
+        return res.status(400).json({
+            success:false,
+            message:"cant process payment"
+        })
     }
    
     
     //order create
+    const amount=coursedetails.price
+    const currency="INR"
+    const options={
+        amount:amount*100,
+        currency:currency,
+        reciept:Math.random(Date.now().toString()),
+        notes:{//that will be used while verifying the signature
+            courseid:courseid,
+            userid,
+        }
+    }
+    try{
+        const paymentresponse=await instance.orders.create(options);
+        console.log(paymentresponse)
+    }catch(err){
+        return res.status(400).json({
+            success:false,
+            message:"cant process order creation"
+        })
+    }
     //response
+    return res.status(200).json({
+        success:true,
+        message:"successfully created order"
+    })
+
+
     }catch(err){
         return res.status(400).json({
             success:false,
             message:err.message
         })
     }
+}
+
+//verify Signature
+
+exports.verifySignature=async(req,res)=>{
+    try{
+        const webhooksecret="123456";
+
+    //fetch encrypted key from razorpay ,,Passed in header
+    const signature=req.header["x-razorpay-signature"];
+    const shasum= crypto.createHmac("sha256",webhooksecret)
+    shasum.update(JSON.stringify(req.body))
+    const digest =shasum.digest("hex")
+
+    if(digest === signature){
+        const{courseid , userid}=req.body.payload.payment.entity.notes;
+        //update course
+        const enrolledcourse=await Course.findOneAndUpdate({_id:courseid},{$push:{StudentsEnrolled:userid},new:true})
+        if(!enrolledcourse){
+            return res.status(400).json({
+                success:false,
+                message:"cant update userid in enrolled courses"
+            })
+        }
+        //update user
+        const enrolleduser=await User.findOneAndUpdate({_id:userid},{$push:{courses:courseid},new:true});
+        if(!enrolleduser){
+            return res.status(400).json({
+                success:false,
+                message:"cant update courseid in enrolled user"
+            })
+        }
+
+        //mailsend
+
+        const emailresponse =await mailSender(enrolleduser.email,"successfully enrolled","successfully enrolled in studynotion website");
+        return res.status(200).json({
+            success:true,
+            message:"successfully verified and course added"
+        })
+    }
+    else{
+        return res.status(400).json({
+            success:false,
+            message:"invalid secret key"
+        })
+    }
+    }catch(err){
+        return res.status(400).json({
+            success:false,
+            message:`cant verify, try again later ,${err.message}`
+        })
+    }
+
 }
